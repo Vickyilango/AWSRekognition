dd226e21cf0324c18375bcb188bc925d5018fd11
Vigneshwar Ilango <ivigneshwar1@gmail.com>
2017-03-13 10:54:20 +0530

    Create readURL.js

---
+++ static/readURL.js
@@ -0,0 +1,16 @@
+function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#blah')
+                        .attr('src', e.target.result)
+                        .width(200)
+                        .height(200);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+                var img = document.getElementById('blah');
+                img.style.visibility = 'visible';
+            }
+        }
